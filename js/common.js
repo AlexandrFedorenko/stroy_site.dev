2ef38536b14eb63a8b1315b91ae1ff16bcaec5ca
AlexandrFedorenko <alexandrfedorenkoooo@gmail.com>
2017-03-25 12:37:49 +0200

    v 0.1

---
+++ js/common.js
@@ -0,0 +1,26 @@
+//$(document).ready(function() {
+//    //footer mob menu
+//	$(".menu-mob-f").click(function () {
+//        $(".foot-nav").slideToggle();
+//    });
+//    //overlay map
+//    $('.overlay').click(function() {
+//		$(this).remove();
+//	});
+//    //sidebar
+//	$(".ac1").click(function (e) {
+//        e.preventDefault();
+//        $(this).toggleClass("sidebar-acor-act")
+//        $(".ul-ac1").slideToggle();
+//    });
+//    $(".ac2").click(function (e) {
+//        e.preventDefault();
+//        $(this).toggleClass("sidebar-acor-act")
+//        $(".ul-ac2").slideToggle();
+//    });
+//    $(".ac3").click(function (e) {
+//        e.preventDefault();
+//        $(this).toggleClass("sidebar-acor-act")
+//        $(".ul-ac3").slideToggle();
+//    });
+//});
